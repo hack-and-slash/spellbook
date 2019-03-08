dddfb42852d68bcaa3d058e83ebd3a3bf94df38c
Victor Miguez <victorcineis@gmail.com>
2019-03-08 10:45:15 -0300

    Updates

---
+++ precache-manifest.6f0ef6e9835152f551ff029ee4d9ebae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e66aaa4b56c88435201",
+    "url": "/static/css/main.7e90eefa.chunk.css"
+  },
+  {
+    "revision": "7e66aaa4b56c88435201",
+    "url": "/static/js/main.7e66aaa4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ea3bdd8c9a6818890153",
+    "url": "/static/js/2.ea3bdd8c.chunk.js"
+  },
+  {
+    "revision": "73c70fe8b95f3aeba30a3b2f8b5d91af",
+    "url": "/index.html"
+  }
+];
